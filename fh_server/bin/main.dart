3aff2b68b4988b344963fd1dbc7e7c1fdab8374c
Dylan Vanhout <123621238+dvanhout89@users.noreply.github.com>
2024-04-04 11:06:34 +0200

    Initialised Server setup
    Initialised server setup with Conduit framework.

---
+++ fh_server/bin/main.dart
@@ -0,0 +1,12 @@
+import 'package:fh_server/fh_server.dart';
+
+Future main() async {
+  final app = Application<FhServerChannel>()
+    ..options.configurationFilePath = "config.yaml"
+    ..options.port = 8888;
+
+  await app.startOnCurrentIsolate();
+
+  print("Application started on port: ${app.options.port}.");
+  print("Use Ctrl-C (SIGINT) to stop running the application.");
+}
---
+++ fh_server/lib/fh_server.dart
@@ -0,0 +1,11 @@
+/// fh_server
+///
+/// A conduit web server.
+library fh_server;
+
+export 'dart:async';
+export 'dart:io';
+
+export 'package:conduit_core/conduit_core.dart';
+
+export 'channel.dart';
